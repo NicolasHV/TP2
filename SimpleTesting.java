2ccc511640f1592cf29513f6034b9d07846d87f1
Nicolas Huet Vasseur <nicolas@huetvasseur.fr>
2024-12-04 17:36:09 +0100

    Changement de nom ET2

--- SimpleTesting.java
+++ SimpleTesting.java
@@ -1,7 +1,7 @@
 public class SimpleTesting {
   public static void main(String[] args) {
-    Student student = new Student("John");
+    Student student = new Student("Etudiant_Temp");
     String name = student.getName();
-  System.out.println("Student name is : "+name);
+  System.out.println("My name is : "+name);
   }
 }
