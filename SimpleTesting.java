9a43f36c7a58727b031a3af538d64abd3692d727
Nicolas Huet Vasseur <nicolas@huetvasseur.fr>
2024-12-04 17:43:52 +0100

    Résolution du conflit

--- SimpleTesting.java
+++ SimpleTesting.java
@@ -1,7 +1,6 @@
 public class SimpleTesting {
   public static void main(String[] args) {
     Student student = new Student("Nicolas");
-    Student student = new Student("Etudiant_Temp");
     String name = student.getName();
   System.out.println("My name is : "+name);
   }
