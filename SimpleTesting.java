4d51ca684be37f934c91987dcba394e042e33f77
NicolasHV <nicolas@huetvasseur.fr>
2024-12-04 17:41:23 +0100

    Update SimpleTesting.java

--- SimpleTesting.java
+++ SimpleTesting.java
@@ -1,10 +1,7 @@
 public class SimpleTesting {
   public static void main(String[] args) {
-<<<<<<< HEAD
     Student student = new Student("Nicolas");
-=======
     Student student = new Student("Etudiant_Temp");
->>>>>>> 2ccc511640f1592cf29513f6034b9d07846d87f1
     String name = student.getName();
   System.out.println("My name is : "+name);
   }
